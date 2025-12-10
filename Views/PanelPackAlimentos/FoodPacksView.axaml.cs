c3e32ecbb9661df005c58a3e972e6bc6a357d974
Jose Noble <128853889+Noble200@users.noreply.github.com>
2025-12-10 12:32:58 -0300

    Add files via upload

--- Views/PanelPackAlimentos/FoodPacksView.axaml.cs
+++ Views/PanelPackAlimentos/FoodPacksView.axaml.cs
@@ -1,17 +1,34 @@
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using Allva.Desktop.ViewModels;
 
-namespace Allva.Desktop.Views.PanelPackAlimentos;
-
-public partial class FoodPacksView : UserControl
+namespace Allva.Desktop.Views.PanelPackAlimentos
 {
-    public FoodPacksView()
+    public partial class FoodPacksView : UserControl
     {
-        InitializeComponent();
-    }
+        public FoodPacksView()
+        {
+            InitializeComponent();
+            // DataContext se asigna desde MainDashboardViewModel
+        }
 
-    private void InitializeComponent()
-    {
-        AvaloniaXamlLoader.Load(this);
+        public FoodPacksView(int idComercio, int idLocal)
+        {
+            InitializeComponent();
+            DataContext = new FoodPacksViewModel(idComercio, idLocal);
+        }
+
+        public FoodPacksView(int idComercio, int idLocal, int idUsuario, 
+                             string nombreUsuario, string numeroUsuario, string codigoLocal)
+        {
+            InitializeComponent();
+            DataContext = new FoodPacksViewModel(idComercio, idLocal, idUsuario, 
+                                                  nombreUsuario, numeroUsuario, codigoLocal);
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
     }
 }
